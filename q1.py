57c598a16f7f5d0a8e0d80e9333291bb39aa09e4
ellechappelle <matheeli@oregonstate.edu>
2026-04-13 12:55:53 -0700

    wrote comments for Q1 and code and comments for Q2

--- q1.py
+++ q1.py
@@ -3,7 +3,14 @@
 # Date: 04/15/2026
 # Description:
 
+# Part one: ask user for a word, store user input as a variable named "word"
 word = input("What is the word that you have in mind?")
+
+# Part two: print the user input 4 times in a row with no spaces
 print(word * 4)
+
+# Part three: print first 3 characters of user input
 print(word[0:3])
+
+# Part four: print user input spelled backwards
 print(word[::-1])
--- q2.py
+++ q2.py
@@ -2,3 +2,18 @@
 # GitHub Username: ellechappelle
 # Date: 04/15/2026
 # Description:
+
+# Part one: store user input of rectangles dimensions as variables "length" and "width"
+length = float(input("Enter the length: "))
+width = float(input("Enter the width: "))
+
+# Part two: compute rectangle characteristics
+area = length * width
+perimeter = (length * 2) + (width * 2)
+diagonal = (length**2 +width**2)**0.5
+
+# Part two: produce summary of rectangle with rounded values
+print("Rectangle summary: ")
+print(f"Area: {round(area, 1)}")
+print(f"Perimeter: {round(perimeter, 1)}")
+print(f"Diagonal: {round(diagonal, 2)}")
